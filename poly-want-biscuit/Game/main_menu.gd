f36db6de812b5bbd799934ff2f53399e702bd30f
Yuval M <108860762+Yuvalm15@users.noreply.github.com>
2025-08-26 18:05:18 +0300

    added "chase" to pirate

--- poly-want-biscuit/Game/main_menu.gd
+++ poly-want-biscuit/Game/main_menu.gd
@@ -1,14 +1,13 @@
 extends Control
+class_name main_menu
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/StartButton.grab_focus()
 
 
 func _on_start_button_pressed() -> void:
-	#get_tree().change_scene("res://Game/game.tscn")
-	pass
+	get_tree().change_scene_to_file("res://Game/game.tscn")
+
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
