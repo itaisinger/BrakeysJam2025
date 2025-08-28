485aa2ec95d8372af0d81eb33971736126775aa5
itaisinger <itai5000si2@gmail.com>
2025-08-28 13:49:01 +0300

    working on hud

--- poly-want-biscuit/Hud/canvas_layer.gd
+++ poly-want-biscuit/Hud/canvas_layer.gd
@@ -1,6 +1,12 @@
 extends CanvasLayer
 var coins=0
 var coins_max = 7
+
+@onready var death
+@onready var win
+
+
+
 func _ready():
 	var root = get_tree().root.get_child(1)
 	#root.connect("add_coin",_add_coin)
@@ -11,4 +17,14 @@ func set_keys(n):
 
 func death_screen():
 	var tween = create_tween()
-	tween.tween_property($ColorRect, "modulate:a", 1.0, 2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property($deathImage, "modulate:a", 1.0, 3).set_trans(Tween.TRANS_SINE).set_custom_interpolator(inter)
+
+func win_screen():
+	var tween = create_tween()
+	tween.tween_property($winImage, "modulate:a", 1.0, 3).set_trans(Tween.TRANS_SINE).set_custom_interpolator(inter)
+
+	
+	
+func inter(x):
+	return min(0.8, pow(x,3))
+	
