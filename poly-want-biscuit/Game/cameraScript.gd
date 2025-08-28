4f51e2f4707b83864fe2fe66c54f26380ca48618
itaisinger <itai5000si2@gmail.com>
2025-08-28 23:30:33 +0300

    camera tweak

--- poly-want-biscuit/Game/cameraScript.gd
+++ poly-want-biscuit/Game/cameraScript.gd
@@ -1,12 +1,18 @@
 extends Camera2D
 
 @onready var player = %Parot
+@onready var target = %camera_target
 @export var xoff : float = 100
 @export var off_spd : float = 0.01
+var current_offset = 0
 
 func _process(delta: float) -> void:
-	offset.x = lerp(offset.x,xoff * player.dir,off_spd)
-	pass
+	current_offset = lerp(float(current_offset),xoff * player.xspd,off_spd)
+	target.position = player.position + Vector2(current_offset,0)
 	
-func lerp(start,end,spd):
-	return start + (end-start)*spd
+	
+	#offset.x = lerp(offset.x,xoff  * player.xspd,off_spd)
+	pass
+
+#func lerp(start: float,end: float,spd:float):
+	#return start + (end-start)*spd
