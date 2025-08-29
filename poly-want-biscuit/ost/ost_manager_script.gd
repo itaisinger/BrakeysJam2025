55bc7f423ba77080dc3bfd84f2d2c9e57543d3bc
itaisinger <itai5000si2@gmail.com>
2025-08-29 13:24:46 +0300

    ost volume

--- poly-want-biscuit/ost/ost_manager_script.gd
+++ poly-want-biscuit/ost/ost_manager_script.gd
@@ -4,9 +4,13 @@ extends Node2D
 @export var menuOst : AudioStream
 @export var gameOst : AudioStream
 @export var isMenu : bool = false
+@export_range(0,1,0.01) var volume : float = 0.5
 @onready var player = $player
 
 func _ready():
 	player.stream = menuOst if isMenu else gameOst
 	player.play()
+	
+func _process(delta: float) -> void:
+	player.volume_db = lerp(-30,0,volume)
 	
